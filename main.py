811135ecd549dbdd2a753e6e7e805fe163432b53
Gill.Singh.A <amansg22@iitk.ac.in>
2025-07-08 23:03:12 +0530

    Implemented Filtering of IP Addresses using STUN Protocol in Packet Capture File

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 from os import geteuid
 from datetime import date
+from ipwhois import IPWhois
+from ipwhois.exceptions import IPDefinedError
 from pyshark import FileCapture
 from optparse import OptionParser
 from scapy.all import get_if_list
@@ -25,14 +27,37 @@ def get_arguments(*args):
         parser.add_option(arg[0], arg[1], dest=arg[2], help=arg[3])
     return parser.parse_args()[0]
 
+stun_ips = set()
+with open("ignore_asn_names.txt", 'r') as file:
+    ignore_asns = [asn_name for asn_name in file.read().split('\n') if asn_name != '']
+
 def check_root():
     return geteuid() == 0
+
+def get_asn_name(ip):
+    return IPWhois(ip).lookup_whois().get("asn_description")
+    
+def process_packet(packet):
+    if "stun" in packet:
+        if packet["ip"].src not in stun_ips:
+            try:
+                asn_name = get_asn_name(packet["ip"].src)
+                return packet["ip"].src, packet["ip"].dst, asn_name, (True if len([asn for asn in ignore_asns if asn in asn_name]) == 0 else False)
+            except IPDefinedError as error:
+                pass
+        if packet["ip"].dst not in stun_ips:
+            try:
+                asn_name = get_asn_name(packet["ip"].dst)
+                return packet["ip"].dst, packet["ip"].src, asn_name, (True if len([asn for asn in ignore_asns if asn in asn_name]) == 0 else False)
+            except IPDefinedError as error:
+                pass
 
 if __name__ == "__main__":
     arguments = get_arguments(('-i', "--iface", "iface", f"Network Interface on which sniffing has to be done ({','.join(get_if_list())})"),
                               ('-v', "--verbose", "verbose", "Display Additional Information related to the STUN Packets on the screen"),
                               ('-w', "--write", "write", "Dump the captured STUN Packets to File"),
                               ('-r', "--read", "read", "Read Packets from a Packet Capture File"))
+    arguments.verbose = True if arguments.verbose and arguments.verbose.lower() == "true" else False
     if arguments.read:
         try:
             packets = FileCapture(arguments.read)
@@ -42,6 +67,18 @@ if __name__ == "__main__":
         except Exception as error:
             display('-', f"Error occured while reading Packets from File {Back.MAGENTA}{arguments.read}{Back.RESET} => {Back.YELLOW}{error}{Back.RESET}")
             exit(0)
+        for packet in packets:
+            info = process_packet(packet)
+            if info == None:
+                continue
+            elif info[3]:
+                stun_ips.add(info[0])
+                display('*', f"Detected STUN Packets from {Back.WHITE}{info[1]}{Back.RESET} => {Back.BLUE}{info[0]} ({info[2]}){Back.RESET}")
+            elif arguments.verbose:
+                stun_ips.add(info[0])
+                display('*', f"{Back.MAGENTA}[VERBOSE: ASN in Ignore List]{Back.RESET} Detected STUN Packets from {Back.WHITE}{info[1]}{Back.RESET} => {Back.BLUE}{info[0]} ({info[2]}){Back.RESET}")
+            else:
+                stun_ips.add(info[0])
     elif not arguments.iface or arguments.iface not in get_if_list():
         display('-', f"Please specify a valid {Back.YELLOW}Network Interface{Back.RESET} for Sniffing")
         exit(0)
